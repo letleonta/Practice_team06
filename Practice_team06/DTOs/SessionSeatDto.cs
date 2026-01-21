bad9de96355c88b2b298a1dc58c09270001898d3
Mykola Petrov <petrov191212@gmail.com>
2026-01-21 20:17:51 +0200

    123

--- Practice_team06/DTOs/SessionSeatDto.cs
+++ Practice_team06/DTOs/SessionSeatDto.cs
@@ -1,13 +1,24 @@
-﻿using Practice_team06.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Practice_team06.Models;
 
 namespace Practice_team06.DTOs;
 
 public class SessionSeatDto
 {
+    [Required]
     public int SeatId { get; set; }
+
+    [Required]
     public short RowNumber { get; set; }
+
+    [Required]
     public short SeatNumber { get; set; }
+
+    [Required]
     public SeatType Type { get; set; }
+
+    [Required]
     public bool IsAvailable { get; set; } 
+    
     public decimal? Price { get; set; }    
 }
