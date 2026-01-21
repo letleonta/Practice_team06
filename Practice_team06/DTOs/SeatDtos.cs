60e73e7b2dd7d49c8ace02d90f52c55df7dd0c24
Mykola Petrov <petrov191212@gmail.com>
2026-01-21 19:52:21 +0200

    Added validation in SeatDtos

--- Practice_team06/DTOs/SeatDtos.cs
+++ Practice_team06/DTOs/SeatDtos.cs
@@ -1,21 +1,37 @@
-﻿using Practice_team06.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Practice_team06.Models;
 
 namespace Practice_team06.DTOs;
 
 public class SeatDto
 {
     public int Id { get; set; }
+    
     public int HallId { get; set; }
+
+    [Required] 
     public short RowNumber { get; set; }
+
+    [Required] 
     public short SeatNumber { get; set; }
+    
     public decimal? PriceModifier { get; set; }
+
+    [Required]
     public SeatType SeatType { get; set; }
+
+    [Required]
     public SeatStatus SeatStatus { get; set; }
 }
 
 public class UpdateSeatDto
 {
+    [Required(ErrorMessage = "Тип місця є обов'язковим")]
     public SeatType SeatType { get; set; }
+
+    [Required(ErrorMessage = "Статус місця є обов'язковим")]
     public SeatStatus SeatStatus { get; set; }
+
+    [Range(0.1, 5.0, ErrorMessage = "Коефіцієнт ціни має бути від 0.1 до 5.0")]
     public decimal? PriceModifier { get; set; }
 }
