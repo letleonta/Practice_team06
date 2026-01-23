455a5fa8addf9279636a997a42febd1ca9afa990
ArtemPridyba <tema14062006@gmail.com>
2026-01-23 14:59:43 +0200

    Resolved Migrations Conflicts

---
+++ Practice_team06/Migrations/20260123125029_ResolveConflicts.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practice_team06.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResolveConflicts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Seats_HallId",
+                table: "Seats");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_HallId_RowNumber_SeatNumber",
+                table: "Seats",
+                columns: new[] { "HallId", "RowNumber", "SeatNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Seats_HallId_RowNumber_SeatNumber",
+                table: "Seats");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_HallId",
+                table: "Seats",
+                column: "HallId");
+        }
+    }
+}
