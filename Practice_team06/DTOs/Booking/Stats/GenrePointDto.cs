238318f4dccc039cac5454773a5889f1a107bdf5
Violetta Leonova <a4050939@gmail.com>
2026-02-04 13:43:01 +0200

    Added statistics for AdminBookings

---
+++ Practice_team06/DTOs/Booking/Stats/GenrePointDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Practice_team06.DTOs.Booking.Stats;
+
+public class GenrePointDto
+{
+    public string GenreName { get; set; } = "";
+    public int Number { get; set; }
+}
---
+++ Practice_team06/DTOs/Booking/Stats/RevenuePointDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practice_team06.DTOs.Booking.Stats;
+
+public class RevenuePointDto
+{
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+}
