366151a5b34e66b945f84b0aa106cb98ae04ac44
Violetta Leonova <a4050939@gmail.com>
2026-02-04 19:19:35 +0200

    Added movie filtration to MovieService

---
+++ Practice_team06/DTOs/Movie/MovieFilterDto.cs
@@ -0,0 +1,18 @@
+﻿using Practice_team06.Models;
+
+namespace Practice_team06.DTOs.Movie;
+
+public class MovieFilterDto
+{
+    public string? Title { get; set; } = null!;
+    public decimal? Rating { get; set; }
+    public SelectionType? SelectionType { get; set; }
+    public List<AgeRestriction> AgeRestrictions  { get; set; } = new();
+    public List<string> Genres { get; set; } = new();
+}
+
+public enum SelectionType
+{
+    NowPlaying = 0,
+    Upcoming = 1
+}
